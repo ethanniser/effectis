6de0ef418f9ce411acf01c657f3535108ab75100
Ethan Niser <ethan@niser.com>
2025-01-25 22:04:12 -0500

    before remove rimr

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,10 +1,41 @@
-import type { Effect } from "effect"
-import { Context } from "effect"
+import { Context, Effect, Layer, Option, Schema } from "effect"
 import type { Command } from "./Command.js"
-import type { RIMR } from "./RIMR.js"
+import { RIMR } from "./RIMR.js"
+
+export class StorageError extends Schema.TaggedError<StorageError>("StorageError")("StorageError", {
+  message: Schema.String
+}) {}
 
 export interface StorageImpl {
-  run(command: Command): Effect.Effect<RIMR.Value, never>
+  run(command: Command): Effect.Effect<RIMR.Value, StorageError, never>
 }
 
 export class Storage extends Context.Tag("Storage")<Storage, StorageImpl>() {}
+
+export const BasicLive = Layer.effect(
+  Storage,
+  Effect.gen(function*() {
+    const hm = new Map<string, string>()
+    yield* Effect.void
+    return {
+      run: (command) =>
+        Effect.gen(function*() {
+          yield* Effect.void
+          switch (command._tag) {
+            case "Set": {
+              hm.set(command.key.value, command.value.value)
+              return Option.some(new RIMR.String({ value: "OK" }))
+            }
+            case "Get": {
+              const value = hm.get(command.key.value)
+              if (value === undefined) {
+                return new RIMR.String({ value: null })
+              } else {
+                return new RIMR.String({ value })
+              }
+            }
+          }
+        })
+    }
+  })
+)
--- src/bin.ts
+++ src/bin.ts
@@ -5,8 +5,10 @@ import * as NodeContext from "@effect/platform-node/NodeContext"
 import * as NodeRuntime from "@effect/platform-node/NodeRuntime"
 import * as Effect from "effect/Effect"
 import { run } from "./Cli.js"
+import { BasicLive } from "./Storage.js"
 
 run(process.argv).pipe(
+  Effect.provide(BasicLive),
   Effect.provide(NodeSocketServer.layer({ port: 6379 })),
   Effect.provide(NodeContext.layer),
   NodeRuntime.runMain()
