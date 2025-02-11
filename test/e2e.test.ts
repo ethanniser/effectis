2f50c370164e322eb152cef8e861e8b979bd7960
Ethan Niser <ethan@niser.com>
2025-02-11 02:05:44 -0500

    remove multi from e2e tests

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -22,9 +22,9 @@ const sharedServices = pipe(
       NodeSocketServer.layer({ port: 6379 }),
       NodeContext.layer
     )
-  ),
+  )
   // by default the logger is removed from the test context
-  Layer.provide(Logger.pretty)
+  // Layer.provide(Logger.pretty)
 )
 
 const generateKey = Random.nextInt.pipe(Effect.map((i) => `redisTests:${i}`))
@@ -66,95 +66,72 @@ layer(Layer.mergeAll(redisServerLive, redisClientLive), {})(
       Effect.gen(function*() {
         const client = yield* Redis.Redis
         const key = yield* generateKey
-        const results = yield* client.use((client) => client.multi().set(key, "value").get(key).exec())
-        expect(results[1]).toEqual("value")
+        yield* client.use((client) => client.set(key, "value"))
+        const result = yield* client.use((client) => client.get(key))
+        expect(result).toEqual("value")
       }))
 
     it.effect("SET with EX", () =>
       Effect.gen(function*() {
         const client = yield* Redis.Redis
         const key = yield* generateKey
-        const results = yield* client.use((client) =>
-          client.multi()
-            .set(key, "value", { PX: 100 })
-            .get(key)
-            .exec()
-        )
-        expect(results[1]).toEqual("value")
-        yield* sleep("100 millis")
-        const results2 = yield* client.use((client) => client.get(key))
-        expect(results2).toEqual(null)
+        yield* client.use((client) => client.set(key, "value", { PX: 100 }))
+        const result = yield* client.use((client) => client.get(key))
+        expect(result).toEqual("value")
+        yield* sleep("110 millis")
+        const result2 = yield* client.use((client) => client.get(key))
+        expect(result2).toEqual(null)
       }))
 
     it.effect("SET with NX (only if not exists)", () =>
       Effect.gen(function*() {
         const client = yield* Redis.Redis
         const key = yield* generateKey
-        const results = yield* client.use((client) =>
-          client.multi()
-            .set(key, "value1")
-            .set(key, "value2", { NX: true })
-            .get(key)
-            .exec()
-        )
-        expect(results[2]).toEqual("value1")
+        yield* client.use((client) => client.set(key, "value1"))
+        yield* client.use((client) => client.set(key, "value2", { NX: true }))
+        const result = yield* client.use((client) => client.get(key))
+        expect(result).toEqual("value1")
       }))
 
     it.effect("SET with XX (only if exists)", () =>
       Effect.gen(function*() {
         const client = yield* Redis.Redis
         const key = yield* generateKey
-        const results = yield* client.use((client) =>
-          client.multi()
-            .set(key, "value1", { XX: true })
-            .set(key, "value2")
-            .set(key, "value3", { XX: true })
-            .get(key)
-            .exec()
-        )
-        expect(results[3]).toEqual("value3")
+        yield* client.use((client) => client.set(key, "value1", { XX: true }))
+        yield* client.use((client) => client.set(key, "value2"))
+        yield* client.use((client) => client.set(key, "value3", { XX: true }))
+        const result = yield* client.use((client) => client.get(key))
+        expect(result).toEqual("value3")
       }))
 
     it.effect("DEL", () =>
       Effect.gen(function*() {
         const client = yield* Redis.Redis
         const [key1, key2] = yield* Effect.all([generateKey, generateKey])
-        const results = yield* client.use((client) =>
-          client.multi()
-            .set(key1, "value")
-            .set(key2, "value2")
-            .del([key1, key2])
-            .get(key1)
-            .exec()
-        )
-        expect(results[3]).toEqual(null)
+        yield* client.use((client) => client.set(key1, "value"))
+        yield* client.use((client) => client.set(key2, "value2"))
+        yield* client.use((client) => client.del([key1, key2]))
+        const result = yield* client.use((client) => client.get(key1))
+        expect(result).toEqual(null)
       }))
 
     it.effect("EXISTS", () =>
       Effect.gen(function*() {
         const client = yield* Redis.Redis
         const [key1, key2] = yield* Effect.all([generateKey, generateKey])
-        const results = yield* client.use((client) =>
-          client.multi()
-            .set(key1, "value")
-            .set(key2, "value2")
-            .exists([key1, key2])
-            .exec()
-        )
-        expect(results[2]).toEqual(2)
+        yield* client.use((client) => client.set(key1, "value"))
+        yield* client.use((client) => client.set(key2, "value2"))
+        const result = yield* client.use((client) => client.exists([key1, key2]))
+        expect(result).toEqual(2)
       }))
 
     it.effect("TYPE", () =>
       Effect.gen(function*() {
         const client = yield* Redis.Redis
         const key = yield* generateKey
-        const results = yield* client.use((client) =>
-          client.multi()
-            .set(key, "value")
-            .type(key)
-            .exec()
-        )
-        expect(results[1]).toEqual("string")
+        yield* client.use((client) => client.set(key, "value"))
+        const result = yield* client.use((client) => client.type(key))
+        expect(result).toEqual("string")
       }))
   }
 )
